aff32873f50c474345fd92aa5f086d34aafef0fa
abdullah001111 <143931418+abdullah001111@users.noreply.github.com>
2023-12-28 19:06:36 +0600

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -107,11 +107,11 @@ async def start(c: Gojo, m: Message):
                 
         try:
             cpt = f"""
-Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Gojo ✨.
+Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Shouyou Hinata ✨.
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](https://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join [Manga Mayhem](https://t.me/Manga_Mayhem) to read Manga, Manhwa, Pornhwa for free 😁"""
 
             await m.reply_photo(
                 photo=str(choice(StartPic)),
@@ -146,11 +146,11 @@ Join my [News Channel](https://t.me/gojo_bots_network) to get information on all
 async def start_back(_, q: CallbackQuery):
     try:
         cpt = f"""
-Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Gojo ✨.
+Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Shouyou Hinata ✨.
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](http://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join [Manga Mayhem](http://t.me/Manga_Mayhem) to read Manga, Manhwa, Pornhwa for free 😁"""
 
         await q.edit_message_caption(
             caption=cpt,
@@ -241,7 +241,7 @@ async def help_menu(_, m: Message):
             ou = await gen_cmds_kb(m)
             keyboard = ikb(ou, True)
             msg = f"""
-Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Gojo✨.
+Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Shouyou Hinata ✨.
 I'm here to help you manage your group(s)!
 Commands available:
 × /start: Start the bot
